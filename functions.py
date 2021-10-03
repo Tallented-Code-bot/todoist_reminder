fcf4fe0812ce59180ae9ce6e4a15a65c1ca5c7a0
Calvin Tallent <calvintallent@gmail.com>
2021-10-02 17:45:43 -0700

    Add a function to compare datetimes

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-from datetime import datetime,timezone 
+from datetime import datetime,timezone
 from dateutil import parser
 def get_date_object(date_string):
 	"""Parse a datetime string as a datetime object."""
@@ -8,3 +8,10 @@ def get_date_object(date_string):
 def get_today():
 	"""Return the current date and time in a datetime object with timezone."""
 	return datetime.now(timezone.utc).astimezone().replace(microsecond=0)
+
+def is_one_datetime_before_another(one,two):
+	"""Checks if one datetime is before another datetime."""
+	difference=one-two
+	if difference.days<0:
+		return True
+	return False
