662af975abcc291017b43476de43973aad555558
Calvin Tallent <calvintallentspam@gmail.com>
2022-01-25 08:06:38 -0800

    Work on stuff

--- canvas.py
+++ canvas.py
@@ -18,6 +18,14 @@ def get_current_courses():
 		}
 	).json()
 
+def get_current_user():
+	return requests.get(
+		"https://rsd.instructure.com/api/v1/users/self",
+		headers={
+			"Authorization":f"Bearer {CANVAS_TOKEN}"
+		}
+	).json()
+
 def get_current_assignments(course):
 	"""Get the current assignments for a course."""
 	today=functions.get_today()
@@ -40,13 +48,22 @@ def get_current_assignments(course):
 	locations=config["course_locations"][i]
 	tasks=todoist.get_tasks_in_project(locations["todoist_project_id"])
 	for assignment in assignments:
-		print("testing assignment...")
+		print()
+		print(f"testing assignment {assignment['name']}...")
 		if functions.is_value_in_list(assignment["name"],"content",tasks):
 			print(f"Todoist task {assignment['name']} already exists, skipping...")	
 			continue
-		if "submission" in assignment:
-			print(f"Canvas assignment {assignment['name']} has been completed, skipping...")
-			continue
+#		if "submission" in assignment:
+#			#print(assignment["submission"])
+#			if assignment["submission"]["workflow_state"]=="unsubmitted":
+#				pass
+#			print(f"Submission user id is {assignment['submission']['user_id']}")
+#			if assignment["submission"]["user_id"]==config["canvas_user_id"]:
+#				print("Correct user in submission object")
+#			else:
+#				print("Incorrect user in submission object")
+#			print(f"Canvas assignment {assignment['name']} has been completed, skipping...")
+#			continue
 		assignment_due=functions.get_date_object(assignment["due_at"])
 		if functions.is_one_datetime_before_another(today, assignment_due):
 			# If the due date is in the future
@@ -74,6 +91,13 @@ def get_current_assignments(course):
 # course=get_current_courses()[0]["id"]
 # print(todoist.create_todoist_task("This is a test",priority=3))
 # print(get_current_assignments(course))
+
 courses=get_current_courses()
 for course in courses:
 	get_current_assignments(course)
+
+#user=get_current_user()
+#print(user)
+
+	
+#print(f"Current user id equals user in config.json: {user['id']==config['canvas_user_id']}")
